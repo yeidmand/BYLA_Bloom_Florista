c2e590c0b3748cfd819248f4ef7e464d83064d4f
Luís <a113231@uminho.pt>
2025-12-13 17:15:52 +0000

    docs: atualizar readme

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import os 
+import os
 
 FILE_CLIENTS = "login_client.csv"
 FILE_STAFF = "user_work_profil.csv"
@@ -9,36 +9,41 @@ FILE_COMPLAINTS = "complaints.csv"
 FILE_ENEVENTS = "order_events.csv"
 FILE_ORDERS_ITEMS = "order_items.csv"
 
+
 def load_all_users_for_login():
     all_users = []
 
     if os.path.exists(FILE_CLIENTS):
         try:
-            df=pd.read_csv(FILE_CLIENTS, sep=';', dtype=str)
+            df = pd.read_csv(FILE_CLIENTS, sep=";", dtype=str)
             for _, row in df.iterrows():
-                all_users.append({
-                    "id": str(row["contact"]),
-                    "pass": str(row["password"]),
-                    "role": "client",
-                    "name": str(row["name"])
-                })
+                all_users.append(
+                    {
+                        "id": str(row["contact"]),
+                        "pass": str(row["password"]),
+                        "role": "client",
+                        "name": str(row["name"]),
+                    }
+                )
         except Exception as e:
             print(f"Error read clients: {e}")
 
     if os.path.exists(FILE_STAFF):
         try:
-            df = pd.read_csv(FILE_STAFF, sep=';', dtype=str)
+            df = pd.read_csv(FILE_STAFF, sep=";", dtype=str)
             for _, row in df.iterrows():
                 user_role = "estafeta"
                 if "Gestor" in str(row["dutyArea"]):
                     user_role = "manager"
-                
-                all_users.append({
-                    "id": str(row["login"]),
-                    "pass": str(row["password"]),
-                    "role": user_role,
-                    "name": str(row["login"])
-                })
+
+                all_users.append(
+                    {
+                        "id": str(row["login"]),
+                        "pass": str(row["password"]),
+                        "role": user_role,
+                        "name": str(row["login"]),
+                    }
+                )
         except Exception as e:
             print(f"⚠ Lỗi đọc Staff: {e}")
 
@@ -47,63 +52,74 @@ def load_all_users_for_login():
 
 def load_products():
     if os.path.exists(FILE_PRODUCTS):
-        return pd.read_csv(FILE_PRODUCTS, sep=';', dtype=str)
+        return pd.read_csv(FILE_PRODUCTS, sep=";", dtype=str)
     return pd.DataFrame()
 
+
 def save_products(df):
-    df.to_csv(FILE_PRODUCTS, index=False, sep=';')
+    df.to_csv(FILE_PRODUCTS, index=False, sep=";")
+
 
 def load_orders():
     if os.path.exists(FILE_ORDERS):
-        return pd.read_csv(FILE_ORDERS, sep=';')
-    return pd.DataFrame(columns= [
-    "order_id",
-    "id_client",
-    "name",
-    "contact",
-    "address",
-    "ZP1",
-    "ZP2",
-    "order_status",
-    "tracking_number",
-    "id_worker"
-])
+        return pd.read_csv(FILE_ORDERS, sep=";")
+    return pd.DataFrame(
+        columns=[
+            "order_id",
+            "id_client",
+            "name",
+            "contact",
+            "address",
+            "ZP1",
+            "ZP2",
+            "order_status",
+            "tracking_number",
+            "id_worker",
+        ]
+    )
+
 
 def save_orders(df):
-    df.to_csv(FILE_ORDERS, index=False, sep=';')
+    df.to_csv(FILE_ORDERS, index=False, sep=";")
+
 
 def load_complaints():
     if os.path.exists(FILE_COMPLAINTS):
-        return pd.read_csv(FILE_COMPLAINTS, sep=';')
+        return pd.read_csv(FILE_COMPLAINTS, sep=";")
     return pd.DataFrame(columns=["order_id", "content"])
 
+
 def save_complaints(df):
-    df.to_csv(FILE_COMPLAINTS, index=False, sep=';')
+    df.to_csv(FILE_COMPLAINTS, index=False, sep=";")
+
 
 def load_order_events():
     if os.path.exists(FILE_ENEVENTS):
-        return pd.read_csv(FILE_ENEVENTS, sep=';')
-    return pd.DataFrame(columns=[
-        "event_id",
-        "order_id",
-        "order_status",
-        "timestamp",
-        "login",
-        "reason"
-    ])
+        return pd.read_csv(FILE_ENEVENTS, sep=";")
+    return pd.DataFrame(
+        columns=[
+            "event_id", "order_id", "order_status",
+            "timestamp", "login", "reason"
+        ]
+    )
+
+
 def save_order_events(df):
-    df.to_csv(FILE_ENEVENTS, index=False, sep=';')
+    df.to_csv(FILE_ENEVENTS, index=False, sep=";")
+
 
 def load_order_items():
     if os.path.exists(FILE_ORDERS_ITEMS):
-        return pd.read_csv(FILE_ORDERS_ITEMS, sep=';', dtype={'product_id': str})
-    return pd.DataFrame(columns=[
-        "order_id",
-        "product_id",
-        "quantity_ordered",
-        "price_unit",
-        "subtotal"
-    ])
+        return pd.read_csv(
+            FILE_ORDERS_ITEMS, sep=";", dtype={"product_id": str}
+        )
+    return pd.DataFrame(
+        columns=[
+            "order_id", "product_id", "quantity_ordered",
+            "price_unit", "subtotal"
+        ]
+    )
+
+
 def save_order_items(df):
-    df.to_csv(FILE_ORDERS_ITEMS, index=False, sep=';')
-    
+    df.to_csv(FILE_ORDERS_ITEMS, index=False, sep=";")
